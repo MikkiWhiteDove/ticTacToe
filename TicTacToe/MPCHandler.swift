fdc096d1cc294842a490c58b58d2244a3b159008
mikki <mishanabelov2015@icloud.com>
2023-09-27 19:56:08 +0300

    first commit

---
+++ TicTacToe/MPCHandler.swift
@@ -0,0 +1,12 @@
+//
+//  MPCHandler.swift
+//  TicTacToe
+//
+//  Created by Mishana on 13.09.2023.
+//
+
+import UIKit
+
+class MPCHandler: NSObject {
+
+}
---
+++ TicTacToe/TTTImageView.swift
@@ -0,0 +1,20 @@
+//
+//  TTTImageView.swift
+//  TicTacToe
+//
+//  Created by Mishana on 13.09.2023.
+//
+
+import UIKit
+
+class TTTImageView: UIImageView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
